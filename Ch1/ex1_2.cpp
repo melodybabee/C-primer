af4b17c37016fde84f4fff77c327c816e91860dd
Shuai WANG <shuaiwang0121@gmail.com>
2018-07-04 22:00:38 +0800

    update

--- Ch1/ex1_2.cpp
+++ Ch1/ex1_2.cpp
@@ -4,3 +4,4 @@ int main(){
 //命令行编译 $ cc 文件名.ccp or g++ 文件名.cpp(常用)
 //执行 $ a.out or ./a.out(可执行文件在当前目录中)
 //调用 $ echo $? 可以在终端打出返回值
+//在终端调用返回值后回显示255，原因：http://www.tldp.org/LDP/abs/html/exitcodes.html
