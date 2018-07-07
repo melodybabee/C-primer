ee4710fba1f610c839ab0b4c32012ad8d517f4d7
Shuai WANG <shuaiwang0121@gmail.com>
2018-07-07 18:29:24 +0800

    update

--- Ch2/ex2_04.cpp
+++ Ch2/ex2_04.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	unsigned u =10, u2 = 42;
 	std::cout << u2 - u << std ::endl;
-	std::cout << u - u2 << std ::endl;
+	std::cout << u - u2 << std ::endl;//4294967296-32=4294967264
 
 	int i = 10, i2 = 42;
 	std::cout << i2 - i << std ::endl;
@@ -12,3 +12,6 @@ int main()
 	std::cout << i - u << std ::endl;
 	std::cout << u - i << std ::endl;	
 }
+
+//unsigned number 是大于和等于0的数字
+//当unsigned 遇到负数后，会用4294967296减去这个负数的绝对值
