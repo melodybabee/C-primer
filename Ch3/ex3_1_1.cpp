726876e600be5e6dc1b7c811c76ad7ed6b0d5237
Shuai WANG <shuaiwang0121@gmail.com>
2018-07-11 21:38:47 +0800

    update

--- Ch3/ex3_1_1.cpp
+++ Ch3/ex3_1_1.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+using std::cin;
+using std:: cout;
+using std::endl;
 
 int main()
 {
-	std :: cout << "please input two numbers" << std::endl;
+	cout << "please input two numbers" << endl;
 	int a;
 	int b;
-	std::cin >> a;
-	std::cin >> b;
+	cin >> a;
+	cin >> b;
 
 	if	(a > b){//此部分进行数字大小的交换,i为中间变量，先用其中一个变量（a)给中间变量赋值，原有的两个变量交换，再将中间变量的值赋给另一个变量（b)
 	int i = a;
 	a = b;
 	b = i;
+
 	}
 
 	while (a <= b ){
-		std ::cout << a;
+		cout << a;
 		++a; 
 	}
 
 	return 0;
 }
-
-//注意考虑输入数字大小的问题
